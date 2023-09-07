3c75b7377369bbe06199e201950eeb6c10a263b2
Branko Tosin <branko@citizenshipper.com>
2023-09-07 17:29:26 +0200

    Add boilerplate code

---
+++ src/transport-need/other.tsx
@@ -0,0 +1,17 @@
+import * as React from "react"
+
+export const OtherIcon = () => {
+  return (
+    <svg
+      width="25"
+      height="25"
+      viewBox="0 0 25 25"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <circle cx="7.40137" cy="12.8711" r="1" fill="black" />
+      <circle cx="12.4014" cy="12.8711" r="1" fill="black" />
+      <circle cx="17.4014" cy="12.8711" r="1" fill="black" />
+    </svg>
+  )
+}
